ff2e43afd1198ef03cc339c61cce530a3ef95960
douglastenfen <douglastenfen@gmail.com>
2024-01-05 13:15:22 -0300

    feat(header): add initial header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,21 @@
 import Link from "next/link";
+import { Home } from "lucide-react";
 
 const Header = () => {
   return (
     <header className="flex items-center justify-center p-5 text-white">
       <nav>
-        <ul className="flex space-x-4 text-white">
-          <li className="text-white">
-            <Link className="text-white" href="/">
-              About Me
+        <ul className="flex space-x-4">
+          <li className="rounded-md border-2 border-purple px-5 py-3 hover:bg-purple">
+            <Link href="/">About Me</Link>
+          </li>
+          <li className="rounded-md border-2 border-purple px-5 py-3 hover:bg-purple">
+            <Link href="/">
+              <Home />
             </Link>
           </li>
-          <li>
-            <Link href="/contact">Contact</Link>
+          <li className="rounded-md border-2 border-purple px-5 py-3 hover:bg-purple">
+            <Link href="/">Contact</Link>
           </li>
         </ul>
       </nav>
