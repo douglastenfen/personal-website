c32846e1a9c6af532f64659aa5a1a1f1a84bb39c
douglastenfen <douglastenfen@gmail.com>
2024-01-08 21:27:58 -0300

    feat(footer): add footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="pb-7 text-center text-xs text-white">
+      Douglas Tenfen de Oliveira © {new Date().getFullYear()}
+    </footer>
+  );
+}
