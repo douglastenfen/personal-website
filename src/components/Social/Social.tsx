dfa5863f4f7e83f4544e106f702760df516846ae
douglastenfen <douglastenfen@gmail.com>
2024-01-09 22:40:45 -0300

    feat(Social): add hover on social buttons

--- src/components/Social/Social.tsx
+++ src/components/Social/Social.tsx
@@ -6,7 +6,13 @@ export default function Social() {
   return (
     <div className="flex space-x-3">
       {socialMedias.map(({ href, icon: Icon, label }) => (
-        <a href={href} key={label} target="_blank" rel="noopener noreferrer">
+        <a
+          href={href}
+          key={label}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="transition-colors duration-200 ease-in-out hover:text-purple"
+        >
           <Icon />
         </a>
       ))}
